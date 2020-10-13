4ba6f21d46ae8e47b327a2d8dc71c29afd9a444d
13027730830 <wangchen1@zhongguancun.com.cn>
2020-10-13 11:18:15 +0800

    增加filebeat->es->kibina测试

--- src/test/java/com/cc/elasticsearch/ProductRepositoryTest.java
+++ src/test/java/com/cc/elasticsearch/ProductRepositoryTest.java
@@ -4,6 +4,7 @@ import com.cc.elasticsearch.es.ESOrderDO;
 import com.cc.elasticsearch.es.ESProductDO;
 import com.cc.elasticsearch.es.OrderRepository;
 import com.cc.elasticsearch.es.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ElasticSearchApplication.class)
+@Slf4j
 public class ProductRepositoryTest {
 
     @Autowired
@@ -32,12 +34,13 @@ public class ProductRepositoryTest {
     @Test // 插入一条记录
     public void testInsert() {
         ESProductDO product = new ESProductDO();
-        product.setId(1); // 一般 ES 的 ID 编号，使用 DB 数据对应的编号。这里，先写死
+        product.setId(3); // 一般 ES 的 ID 编号，使用 DB 数据对应的编号。这里，先写死
         product.setName("芋道源码");
         product.setSellPoint("愿半生编码，如一生老友");
         product.setDescription("我只是一个描述");
         product.setCid(1);
         product.setCategoryName("技术");
+        log.info("Insert one record into es: "+product);
         productRepository.save(product);
     }
 
@@ -54,11 +57,13 @@ public class ProductRepositoryTest {
 
     @Test // 根据 ID 编号，删除一条记录
     public void testDelete() {
+        log.info("Delete one record from es");
         productRepository.deleteById(1);
     }
 
     @Test // 根据 ID 编号，查询一条记录
     public void testSelectById() {
+        log.info("select one record from es");
         Optional<ESProductDO> userDO = productRepository.findById(1);
         System.out.println(userDO.get());
     }
@@ -70,17 +75,19 @@ public class ProductRepositoryTest {
     }
 
     @Test
-    public void testSearch(){
+    public void testSaveESOrder(){
         ESOrderDO orderDO = new ESOrderDO();
         orderDO.setCreateTime(new Date());
         orderDO.setFlag(true);
         orderDO.setId(5);
         orderDO.setTests(Arrays.asList("a","bff","c"));
+        log.info("save esOrder: "+orderDO);
         orderRepository.save(orderDO);
     }
 
     @Test
     public void testSearch1(){
+        log.info("Search All esOrders");
         Iterable<ESOrderDO> allById = orderRepository.findAllById(Arrays.asList(1, 2, 3, 4, 5));
         allById.forEach(System.out::println);
     }
